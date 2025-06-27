d23602681a6d396349d3d635761f921edc1e90b2
Tayanna Amorim <amorim.tayanna@gmail.com>
2025-06-27 11:11:56 -0300

    :tada: Commit inicial

---
+++ main.py
@@ -0,0 +1,48 @@
+class ElementLinkedList:
+  def __init__(self, id_number, color):
+    self.id_number = id_number
+    self.color = color
+    self.next = None
+    
+  def __repr__(self):
+    return f'[{self.color}, {self.id_number}]'
+    
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.card_green = 1
+    self.card_yellow = 201
+    
+  def inserirSemPrioridade (self, nodo):
+    pass
+  def inserir(self):
+    numero_do_paciente = None
+    color = str(input("Informe a cor do cartão (A/V): ")).upper()
+    
+    if color == 'V':
+      numero_do_paciente = self.card_green
+      self.card_green += 1
+    elif color == 'A':
+      numero_do_paciente = self.card_yellow 
+      self.card_yellow  += 1
+    print(f"Número do cartão: {numero_do_paciente}")
+    nodo = ElementLinkedList(numero_do_paciente, color)
+    
+    if self.head is None:
+      self.head = nodo
+    if color == 'V':
+      self.inserirSemPrioridade(nodo)
+
+
+  
+novo_paciente = LinkedList()
+    
+while True:
+  print("1 - Adicionar paciente a fila")
+  print("2 - Mostrar pacientes na fila")
+  print(f"3 - Chamar paciente\n")
+  option_user = int(input(">> "))
+  if option_user == 1:
+    novo_paciente.inserir()
+  else:
+    break
